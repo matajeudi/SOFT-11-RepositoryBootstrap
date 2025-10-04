b547e8f32fc0cb174932471191481ae84d85a711
matajeudi <jacunam@ucenfotec.ac.cr>
2025-10-04 10:23:19 -0600

    [New] Vue structure and some bootstrap implementation

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+import { RouterView } from 'vue-router';
+
+</script>
+<template>
+  <div class="app-container">
+    <!-- Header y Nav siempre visibles -->
+    <div class="container-fluid">
+      <div class="row bg-light py-3 mb-4">
+        <header class="col-12 text-center mb-0">
+          <h1 class="headerTitle">Implementacion Web con Bootstrap</h1>
+        </header>
+        <nav class="col-12">
+          <ul class="nav justify-content-center">
+            <li class="nav-item"><router-link to="/home" class="nav-link">Inicio</router-link></li>
+            <li class="nav-item"><router-link to="/information" class="nav-link">Informacion</router-link></li>
+            <li class="nav-item"><router-link to="/services" class="nav-link">Servicios</router-link></li>
+            <li class="nav-item"><router-link to="/contact" class="nav-link">Contacto</router-link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <!-- Contenido dinámico de cada vista -->
+    <RouterView />
+
+    <!-- Footer siempre visible -->
+    <div class="container-fluid">
+      <footer class="row">
+        <div class="col-12 text-center mt-4">
+          <p class="footerText">© 2025 UCENFOTEC - EXPO 2 [SOFT-11] / Jeudi Mata. Todos los derechos reservados.</p>
+        </div>
+      </footer>
+    </div>
+  </div>
+
+</template>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
+@import './styleSheet/customCSS.css';
+</style>
+
+
---
+++ src/views/contactPage.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="mainInventario wireframe">
+    <div class="sidebarInventario wireframe"> Sidebar </div>
+    <div class="inventarioTable wireframe"> Ventas/historia </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+
+    name: 'contactPage',
+
+}
+
+</script>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
+@import '../styleSheet/customCSS.css';
+</style>
